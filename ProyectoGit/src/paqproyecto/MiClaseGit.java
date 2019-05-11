4836148f110aa40be26d76a597f107d052d2507b
jorge <jorge@DESKTOP-MQ92OVP>
2019-05-11 18:20:01 +0200

    Merge 1

--- ProyectoGit/src/paqproyecto/MiClaseGit.java
+++ ProyectoGit/src/paqproyecto/MiClaseGit.java
@@ -7,11 +7,11 @@ public class MiClaseGit {
 		int numero2=3;
 		
 		System.out.println("1º DAW - Entornos de Desarrollo");
-		System.out.println("7 + 3 es igual a " + (numero1+numero2));
-		System.out.println();
-		System.out.println("7 - 3 es igual a " + (numero1-numero2));
-		System.out.println();
-		System.out.println("7 x 3 es igual a " +(numero1*numero2));
+		System.out.println("El resultado de 7 + 3 es igual a " + (numero1+numero2));
+		
+		System.out.println("El resultado de 7 - 3 es igual a " + (numero1-numero2));
+		
+		System.out.println("El resultado de 7 x 3 es igual a " +(numero1*numero2));
 	
 
 	}
