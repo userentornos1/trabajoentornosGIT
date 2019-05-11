f40c12278ee0850e5b1c5bcf30a8e2657541c887
jorge <jorge@DESKTOP-MQ92OVP>
2019-05-11 17:04:45 +0200

    varias modificaciones en los sysos

--- ProyectoGit/src/paqproyecto/MiClaseGit.java
+++ ProyectoGit/src/paqproyecto/MiClaseGit.java
@@ -3,14 +3,14 @@ package paqproyecto;
 public class MiClaseGit {
 
 	public static void main(String[] args) {
-		
 		int numero1=7;
 		int numero2=3;
 		
-		System.out.println("Entornos de Desarrollo");
+		System.out.println("1º DAW - Entornos de Desarrollo");
 		
-		System.out.println("La suma de 7 y 3 da como resultado: " + (numero1+numero2));
-		System.out.println("La resta de 7 menos 3 da como resultado: " + (numero1-numero2));
+		System.out.println("7 + 3 = " + (numero1+numero2));
+		System.out.println();
+		System.out.println("7 - 3 = " + (numero1-numero2));
 		
 
 	}
