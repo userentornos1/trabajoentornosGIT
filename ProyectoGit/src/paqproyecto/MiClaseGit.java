07fa5cf35be74091974564bb30408993b75e4d2e
jorge <jorge@DESKTOP-MQ92OVP>
2019-05-11 12:15:45 +0200

    Primera versión del proyecto

---
+++ ProyectoGit/src/paqproyecto/MiClaseGit.java
@@ -0,0 +1,16 @@
+package paqproyecto;
+
+public class MiClaseGit {
+
+	public static void main(String[] args) {
+		
+		int numero1=7;
+		int numero2=3;
+		
+		System.out.println("Entornos de Desarrollo");
+		
+		System.out.println("La suma de 7 y 3 da como resultado: " + (numero1+numero2));
+
+	}
+
+}
