3f58e34f73117f54fed42544a590acccd50d5932
jorge <jorge@DESKTOP-MQ92OVP>
2019-05-11 18:47:33 +0200

    Conflicto corregido

--- ProyectoGit/src/paqproyecto/MiClaseGit.java
+++ ProyectoGit/src/paqproyecto/MiClaseGit.java
@@ -7,18 +7,13 @@ public class MiClaseGit {
 		int numero2=3;
 		
 		System.out.println("1º DAW - Entornos de Desarrollo");
+		System.out.println();
 		System.out.println("El resultado de 7 + 3 es igual a " + (numero1+numero2));
-		
+		System.out.println();
 		System.out.println("El resultado de 7 - 3 es igual a " + (numero1-numero2));
-		
+		System.out.println();
 		System.out.println("El resultado de 7 x 3 es igual a " +(numero1*numero2));
-		System.out.println();
-		System.out.println("7 + 3 	Resultado:" + (numero1+numero2));
-		System.out.println();
-		System.out.println("7 - 3	Resultado " + (numero1-numero2));
-		System.out.println();
-		System.out.println("7 x 3 	Resultado " +(numero1*numero2));
-	
+
 
 	}
 
