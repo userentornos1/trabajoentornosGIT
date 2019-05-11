917f9256b76e3c2745c7ed63c222a99d383b2173
jorge <jorge@DESKTOP-MQ92OVP>
2019-05-11 16:45:59 +0200

    Cambio en el orden de los sysos Añadido otro syso

--- ProyectoGit/src/paqproyecto/MiClaseGit.java
+++ ProyectoGit/src/paqproyecto/MiClaseGit.java
@@ -9,8 +9,14 @@ public class MiClaseGit {
 		
 		System.out.println("Entornos de Desarrollo");
 		
-		System.out.println("La suma de 7 y 3 da como resultado: " + (numero1+numero2));
 		System.out.println("La resta de 7 menos 3 da como resultado: " + (numero1-numero2));
+		
+		System.out.println("La suma de 7 y 3 da como resultado: " + (numero1+numero2));
+		
+		System.out.println("El producto de 7 y 3 da como resultado: " + (numero1*numero2));
+		
+		
+
 		
 
 	}
